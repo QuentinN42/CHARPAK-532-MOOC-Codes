f9ba27f891d0fbfc00da9828f8463f402ccaead8
QuentinN42 <quentin.lieumont@gmail.com>
2019-10-03 09:48:52 +0200

    parseHTML and parseTable

--- 2048/brain.js
+++ 2048/brain.js
@@ -1,27 +1,64 @@
 
 document.addEventListener('keydown', function(event) {
     const key = event.key;
-    if (key === "ArrowUp")
-    {
-        let score = document.getElementById("score");
-        score.innerText = parseInt(score.innerText) + 1;
+    switch (key) {
+        case "ArrowUp":
+            let score = document.getElementById("score");
+            score.innerText = parseInt(score.innerText) + 1;
+            break;
+        case "ArrowRight":
+            let tab = [[0,0,0,0],[0,0,0,0],[0,1,0,0],[0,0,0,0]];
+            document.getElementById("main").innerHTML = parseTable(tab);
+            break;
+        case "ArrowLeft":
+            console.log(parseHTML(document.getElementById("main").innerHTML));
+            break;
+        default:
+            console.log(key)
     }
 });
 
-document.addEventListener('click', parseHTML);
-
 function parseHTML(htmlText)
 {
-    /*
-    htmlText = document.getElementById("main").innerHTML;
-    let t1 = [for (x of String(htmlText).split("<tr>")) x.split("<td>")];
-    console.log(t1);
-    */
-    return []
+    let tab = [];
+    htmlText = htmlText.replace(new RegExp("<tbody>","g")," ");
+    htmlText = htmlText.replace(new RegExp("</tbody>","g")," ");
+    htmlText = htmlText.replace(new RegExp("</td>","g")," ");
+    htmlText = htmlText.replace(new RegExp("</tr>","g")," ");
+    htmlText = htmlText.replace(new RegExp("\n","g")," ");
+    htmlText = htmlText.replace(/ /g,'');
+
+    let tmp = htmlText.split("<tr>");
+    tmp.forEach(append);
+
+    function append(str) {
+        tab.push(str.split("<td>"));
+    }
+    tab.shift();
+    let tab2 = [];
+    tab.forEach(append2);
+
+    function append2(line) {
+        line.shift();
+        tab2.push(line);
+    }
+    return tab2
 }
 
 function parseTable(table)
 {
-    // TODO : https://www.w3schools.com/js/js_arrays.asp
-    return ""
+    let output = "";
+    table.forEach(Build);
+
+    function Build(line)
+    {
+        let tmp = "";
+        line.forEach(build);
+        output += "<tr>" + tmp + "</tr>";
+
+        function build(e) {
+            tmp += "<td>" + e + "</td>"
+        }
+    }
+    return output;
 }
